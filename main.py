4d1c8da3bd2f9671ac1efb3546e73beebf71cece
Stuart <40573569+sr-dean@users.noreply.github.com>
2020-10-01 10:42:17 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,41 +1,30 @@
 from kivymd.app import MDApp
-from kivymd.uix.screen import Screen
-from kivymd.uix.datatables import MDDataTable
-from kivy.metrics import dp
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+Window.size = (300,500)
+
+navigation_helper = """
+Screen:
+    NavigationLayout:
+        ScreenManager:
+            Screen:
+                BoxLayout:
+                    orientation: 'vertical'
+                    MDToolbar:
+                        title: 'Demo App'
+                        left_action_items: [["menu",lambda x: nav_drawer.toggle_nav_drawer()]]
+                        elevation: 11
+                    Widget:
+        MDNavigationDrawer: 
+            id: nav_drawer         
+
+"""
 
 
 class DemoApp(MDApp):
     def build(self):
-        screen = Screen()
-        table = MDDataTable(pos_hint=
-                            {'center_x': 0.5, 'center_y': 0.5},
-                            size_hint=(0.9, 0.6),
-                            check=True,
-                            rows_num=10,#allows scrolling
-                            column_data=[("Food", dp(40)),
-                                         ("Calories", dp(15)),
-                                         ("Taste", dp(10))],
-                            row_data=[("Burger", "300", "7"),
-                                      ("Pizza", "720", "8"),
-                                      ("Pizza", "760", "8"),
-                                      ("Pizza", "720", "8"),
-                                      ("Pizza", "770", "8"),
-                                      ("Pizza", "780", "8"),
-                                      ("Pizza", "790", "8"),
-                                      ("Pizza", "710", "8"),
-                                      ("Pizza", "740", "8"),
-                                      ("Pizza", "730", "8")])
-        table.bind(on_check_press=self.check_press)
-        table.bind(on_row_press=self.row_press)
-        screen.add_widget(table)
-
+        screen = Builder.load_string(navigation_helper)
         return screen
-
-    def check_press(self, instance_table, current_row):
-        print(instance_table, current_row)
-
-    def row_press(self, instance_table, current_row):
-        print(instance_table, current_row)
-
 
 DemoApp().run()
